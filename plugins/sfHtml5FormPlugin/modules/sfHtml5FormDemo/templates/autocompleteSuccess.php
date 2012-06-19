8883069c27bd090d24262eefb92d1d902d12bf8a
Michel Parpaillon <michel.parpaillon@gmail.com>
2012-06-19 23:52:28 +0200

    Update submodules

--- plugins/sfHtml5FormPlugin/modules/sfHtml5FormDemo/templates/autocompleteSuccess.php
+++
@@ -1,5 +0,0 @@
-<select xmlns="http://www.w3.org/1999/xhtml">
-<?php foreach($data as $entry):?>
-	<option><?php echo $entry;?></option>
-<?php endforeach;?>
-</select>
--- plugins/sfHtml5FormPlugin/test/unit/widget/sfWidgetFormInputSearchTest.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include(dirname(__FILE__).'/../../bootstrap/unit.php');
-
-$t = new lime_test(1, new lime_output_color());
-
-$w = new sfWidgetFormInputSearch();
-
-$t->is($w->render('foobar'), '<input type="search" name="foobar" id="foobar" />', 'render empty tag ok');
